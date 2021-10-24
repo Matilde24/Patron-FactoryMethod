5a100eb18f740a97d8c70631bd7c2185290e24f8
Matilde24 <matyarostegui@gmail.com>
2021-10-23 19:05:23 -0600

    FactoriaDFiguras

--- src/main/java/org/matilde/FactoriaDFiguras.java
+++ src/main/java/org/matilde/FactoriaDFiguras.java
@@ -1,4 +1,14 @@
 package org.matilde;
 
 public class FactoriaDFiguras {
+
+    public static final int CUADRADO = 0;
+    public static final int CIRCULO = 1;
+    public Figura getFigura(int tipo, int lado) {
+        if (tipo == CUADRADO) {
+            return (new Cuadrado(lado));
+        } else {
+            return (new Circulo(lado));
+        }
+    }
 }
