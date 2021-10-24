08a15d977db0d208ccb65846343e666331be83cf
Matilde24 <matyarostegui@gmail.com>
2021-10-23 19:32:06 -0600

    Figura

--- src/main/java/org/matilde/Figura.java
+++ src/main/java/org/matilde/Figura.java
@@ -3,4 +3,11 @@ package org.matilde;
 import java.awt.*;
 
 public abstract class Figura {
+
+    int lado = 0;
+    public Figura(int lado) {
+        this.lado = lado;
+    }
+    public abstract double getArea();
+    public abstract void dibujar(Graphics g, int x, int y);
 }
